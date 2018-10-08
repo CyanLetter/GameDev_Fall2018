0f76e7142f4a8ca7aa5c97b3253808217f02e5fa
Dakota Ling <ourthinking@gmail.com>
2018-10-08 00:04:56 -0400

    Week 2 slides and assets

---
+++ demos/GameDev_Week2/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = newPos;
+	}
+}
---
+++ demos/GameDev_Week2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D body;
+    public float moveSpeed = 5f;
+    int itemsCollected = 0;
+    int itemsToWin = 5;
+
+    public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        //Vector2 pos = new Vector2(transform.position.x + horizontalInput, transform.position.y + verticalInput);
+        //body.MovePosition(pos);
+
+        Vector2 movement = new Vector2(horizontalInput * moveSpeed, verticalInput * moveSpeed);
+        body.AddForce(movement);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Collectable")) {
+            collision.gameObject.SetActive(false);
+
+            itemsCollected += 1;
+            print(itemsCollected);
+            if (scoreText != null) {
+                scoreText.text = itemsCollected.ToString();
+            }
+
+            if (itemsCollected >= itemsToWin) {
+                OnWin();
+            }
+
+        }
+
+    }
+
+    void OnWin () {
+        print("Success!");
+
+        if (scoreText != null)
+        {
+            scoreText.text = "You win!";
+        }
+    }
+}
